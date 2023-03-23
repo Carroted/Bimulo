496ea93de15581c746cc48c2fe46f278d2c1f83a
aMySour <52638772+Alex-Sour@users.noreply.github.com>
2023-03-23 15:01:34 -0300

    ok

--- index.js
+++ index.js
@@ -1,18 +1,66 @@
 import express from "express";
+import WebSocket from 'ws';
+import nodeDataChannel from 'node-datachannel';
 
 const app = express();
 // make http server (esm import)
 import http from "http";
 const server = http.createServer(app);
-import { Server } from "socket.io";
-const io = new Server(server);
+
+const wss = new WebSocket.Server({ port: 3000 });
+
+wss.on('connection', (ws) => {
+  let peer1 = new nodeDataChannel.PeerConnection('Peer1', { iceServers: ['stun:stun.l.google.com:19302'] });
+  let dc1 = null;
+
+  peer1.onLocalDescription((sdp, type) => {
+    console.log('Peer1 SDP:', sdp, ' Type:', type);
+    ws.send(JSON.stringify({ sdp: sdp, type: type }));
+  });
+
+  peer1.onLocalCandidate((candidate, mid) => {
+    console.log('Peer1 Candidate:', candidate);
+    ws.send(JSON.stringify({ candidate: candidate, mid: mid }));
+  });
+
+  ws.on('message', (message) => {
+    const msg = JSON.parse(message);
+
+    if (msg.sdp) {
+      peer1.setRemoteDescription(msg.sdp, msg.type);
+    } else if (msg.candidate) {
+      peer1.addRemoteCandidate(msg.candidate, msg.mid);
+    }
+  });
+
+  peer1.onDataChannel((dc) => {
+    console.log('Peer1 Got DataChannel: ', dc.getLabel());
+    dc1 = dc;
+    dc1.onMessage((msg) => {
+      console.log('Peer1 Received Msg:', msg);
+    });
+  });
+
+  dc1 = peer1.createDataChannel('test');
+  dc1.onMessage((msg) => {
+    console.log('Peer1 Received Msg dc1:', msg);
+  });
+
+  // Send test message to client after some time
+  setTimeout(() => {
+    if (dc1) {
+      dc1.sendMessage('Hello from Peer1');
+      dc1.sendMessage('Hello from Peer1');
+      dc1.sendMessage('Hello from Peer1');
+    }
+  }, 5000);
+});
 
 import * as url from 'url';
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 import Box2DFactory from "box2d-wasm";
-//import { assertFloatEqual } from './assertFloatEqual.js';
 const box2D = await Box2DFactory();
 /*
   // get verts from body
@@ -51,12 +99,53 @@ const box2D = await Box2DFactory();
     }
   }*/
 
-app.use(express.static("client"));
+// import our webrtc server
+import Peer from "simple-peer";
+import wrtc from "wrtc";
+// we will use peer as a server. yes, its for p2p, but if all clients are only connected to server and not each other, it works for authority multiplayer
 
-// static serve node_modules/@tabler/icons/icons
-app.use("/icons", express.static(__dirname + "/node_modules/@tabler/icons/icons"));
+// create peer server with our existing http server
+const peerServer = new Peer({
+  initiator: true,
+  wrtc: wrtc,
+  trickle: false
+});
+peerServer.on("connect", () => {
+  // webrtc connection
+  // send verts to client
+  console.log("vomit everywhere");
+});
+peerServer.on("signal", (data) => {
+  console.log("signal vomit");
+  // send signal to client
+});
+peerServer.on("data", (data) => {
+  console.log("data vomit");
+  // receive data from client
+});
 
-const frameRate = 1000 / 30;
+var peers = [];
+// ploopy monkey
+const omniPeer = new Peer({
+  initiator: false,
+  wrtc: wrtc,
+  trickle: false
+});
+omniPeer.on("connect", () => {
+  // webrtc connection
+  console.log("OMNI vomit everywhere");
+});
+omniPeer.on("signal", (data) => {
+  console.log("OMNI signal vomit");
+  // send signal to client
+});
+omniPeer.on("data", (data) => {
+  console.log("OMNI data vomit");
+  // receive data from client
+});
+peers.push(omniPeer);
+
+const frameRate = 1000 / 10;
 const canvas = { width: 800, height: 400 };
 const boxes = 20;
 const boxSize = 20;
@@ -83,27 +172,37 @@ body.SetLinearVelocity(zero);
 body.SetAwake(true);
 body.SetEnabled(true);
 
-const timeStepMillis = 1 / 60;
 const velocityIterations = 1;
 const positionIterations = 1;
 
-const iterations = 6;
-for (let i = 0; i < iterations; i++) {
-  const timeElapsedMillis = timeStepMillis * i;
-  {
-    const { y } = body.GetLinearVelocity();
-    console.log(`body.GetLinearVelocity().y = ${y}`);
-    {
-      const { y } = body.GetPosition();
-      console.log(`body.GetPosition().y = ${y}`);
-    }
-  }
-  world.Step(timeStepMillis, velocityIterations, positionIterations);
-}
+app.use(express.static("client"));
+console.log('staticked the client folderation');
 
-console.log(`👍 Ran ${iterations} iterations of a falling body. Body had the expected position on each iteration.`);
+// static serve node_modules/@tabler/icons/icons
+app.use("/icons", express.static(__dirname + "/node_modules/@tabler/icons/icons"));
+
+// put app on http server
+
 
 server.listen(4613, () =>
   console.log("server listening on " + 4613)
 );
 
+// step constantly and send to clients with webrtc
+setInterval(() => {
+  // step physics
+  world.Step(frameRate, velocityIterations, positionIterations);
+  // get body
+  var node = world.GetBodyList();
+  while (box2D.getPointer(node)) {
+    var b = node;
+    node = node.GetNext();
+    var position = b.GetPosition();
+    console.log("position: " + position.x + ", " + position.y);
+
+    // send to clients that are real
+  }
+
+
+  console.log("vomit");
+}, frameRate);
