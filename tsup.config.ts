b2b1f7a3cbf7a70a114eeb8629842ecea085b18d
oneyusof <yusofoyusofo@gmail.com>
2023-07-30 17:59:33 -0400

    lol yes nice mhmm wow nice yeas I added fixed and WOwohoowowowowoow

---
+++ tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+    entry: ["server/**/*.ts", "shared/**/*.ts", "client/**/*.ts"],
+    minify: true,
+    platform: "browser",
+    target: "esnext",
+    bundle: false,
+    format: "esm",
+    outDir: "dist"
+})
